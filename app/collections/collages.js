017fc780b28c30cd274c6122cbfdc68704e5d678
gaarf <onlineallthetime@gmail.com>
2013-06-07 18:29:22 -0700

    identation

--- app/collections/collages.js
+++ app/collections/collages.js
@@ -4,27 +4,36 @@ var Collage = require('../models/collage')
 
 module.exports = Base.extend({
     model: Collage
+
   , api: 'polyvore'
+
   , url: function() {
-        if(!this.options.topic) {
-            return '/cgi/popular.fashion?.out=json';
-        }
-        return '/cgi/splash.topic?.out=json&topic=:topic';
+      var url;
+      if(!this.options.topic) {
+        url = '/cgi/popular.fashion?';
+      }
+      else {
+        url = '/cgi/splash.topic?topic=:topic';
+      }
+      url += '&.out=json';
+      return url;
     }
+
   , parse: function(res) {
-        return _(res.articles || res.result.items)
-            .chain()
-            .filter(function(o){
-                return _.isObject(o) && o.object_class === 'set';
-            })
-            .map(function(o){
-                o.id = o.object_id;
-                o.clickurl = o.clickurl
-                  .replace(/^\.\./, '')
-                  .replace(/set\?id=/, 'set/');
-                return o;
-            })
-            .value();
+      return _(res.articles || res.result.items)
+        .chain()
+        .filter(function(o){
+            return _.isObject(o) && o.object_class === 'set';
+        })
+        .map(function(o){
+            o.id = o.object_id;
+            o.clickurl = o.clickurl
+              .replace(/^\.\./, '')
+              .replace(/set\?id=/, 'set/');
+            return o;
+        })
+        .value();
     }
+
 });
 module.exports.id = 'Collages';
