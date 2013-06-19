bdc5c76999a36c27dd5caf7cb76a9579f5817190
gaarf <onlineallthetime@gmail.com>
2013-06-19 11:02:23 -0700

    various improvements

--- app/controllers/home_controller.js
+++ app/controllers/home_controller.js
@@ -1,19 +1,23 @@
 module.exports = {
   index: function(params, callback) {
 
+    console.log(this.app);
+
   	// only the server side can write to the session
-  	if(this.app.req && this.app.req.session) {
-  		this.app.req.session.homeload = Date.now();
+  	if(this.app.req) {
+
+      if(this.app.req.session) {
+        this.app.req.session.homeload = Date.now();
+      }
 
       console.log('hello server!');
   	}
     else {
-
       console.log('hello client!');
     }
 
-  	console.log('!!!!!!! HELLO WORLD !!!!!!!!!');
 
+  	console.log('!!!!!!! HELLO WORLD !!!!!!!!!');
 
     callback(null, 'home/index');
 
--- app/views/home/index.js
+++ app/views/home/index.js
@@ -2,6 +2,13 @@ var BaseView = require('../base_view');
 
 module.exports = BaseView.extend({
   className: 'home_index_view'
+
+, postRender: function() {
+	this.$el.append(
+		'<p>' + Date.now() + ' <small>this view was hydrated</small></p>'
+	);
+  }
+
 });
 module.exports.id = 'home/index';
 
